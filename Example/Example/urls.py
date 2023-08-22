4a7bbc8f3068d5236b225dfbe3bfd76a0c6703ce
Erdenebayr2 <94421082+Erdenebayr2@users.noreply.github.com>
2023-08-22 18:00:05 +0800

    delete

--- Example/Example/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path,include
-from examapp import views
-
-urlpatterns = [
-    path('chats',include('examapp.urls')),
-    path('', views.userlog, name='userlog'),
-    path('accounts.google.com/', views.google_login, name='google')
-]
